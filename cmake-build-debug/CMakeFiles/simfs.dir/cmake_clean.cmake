9e96053b3dd7d4b416090f76cddb0ba6495f6101
Travis CI <travis@travis-ci.org>
2019-04-09 17:50:06 -0700

    init

---
+++ cmake-build-debug/CMakeFiles/simfs.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simfs.dir/test_simfs.c.o"
+  "CMakeFiles/simfs.dir/simfs.c.o"
+  "bin/simfs.pdb"
+  "bin/simfs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simfs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
